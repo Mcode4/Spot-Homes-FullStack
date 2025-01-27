e6970ce19d167aa06dba989762b8103dc917b6de
Mcode4 <lamllc20@gmail.com>
2025-01-27 15:33:27 -0500

    rating upd

--- frontend/src/components/ManageSpots/ManageSpots.jsx
+++ frontend/src/components/ManageSpots/ManageSpots.jsx
@@ -40,8 +40,7 @@ function ManageSpots(){
                 
                 if(num2 >= 5) num1 += 1
 
-                newValue = `${split[0]}.${num1}` 
-                newValue = Number(newValue)
+                newValue = `${split[0]}.${num1}`
                 
                 spot.displayRating = newValue
             } 
